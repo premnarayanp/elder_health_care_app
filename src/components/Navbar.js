c89f8d2c535ce61a693ad6971aaed60860f834ba
premnarayanp <premnarayanpatel223@gmail.com>
2023-11-25 16:51:43 +0530

    HomePage  and Navbar Router

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/navbar.css'
+
+ function Navbar(){
+
+  return (
+    <div className="nav">
+      <Link to="/">
+        <button className='menuButton'>Home</button>
+      </Link>
+
+      <div className='rounded-img-container'>
+        <img src={require('../assets/myPhoto.jpg')} alt="user-pic" />
+      </div>
+       
+    </div>
+  );
+}
+
+export default Navbar;
